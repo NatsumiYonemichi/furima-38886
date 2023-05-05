1f4dafad3644e6f14707ac4a15d597a37f7ac268
NatsumiYonemichi <123153529+NatsumiYonemichi@users.noreply.github.com>
2023-05-05 14:04:25 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  with_potions presence: true do
+
+  with_options presence: true do
     validates :nickname
     validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
