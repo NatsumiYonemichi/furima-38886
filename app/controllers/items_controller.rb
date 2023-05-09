29383400ff7a775babd012941b1ed73a693eebd7
NatsumiYonemichi <123153529+NatsumiYonemichi@users.noreply.github.com>
2023-05-09 23:41:32 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :category_id, :condition_id, :prefecture_id, :postage_type_id, :shipping_day_id, :image).merge(user_id: current_user.id)
+  end
 end
