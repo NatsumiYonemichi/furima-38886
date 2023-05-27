690e160edf98a4f493c1c07e11d7cbb313b7defc
NatsumiYonemichi <123153529+NatsumiYonemichi@users.noreply.github.com>
2023-05-27 21:33:20 +0900

    フォーム情報保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,25 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: :index
+
   def index
     @payment_purchase = PaymentPurchase.new
+  end
+
+  def create
+    @payment_purchase = PaymentPurchase.new(payment_purchase_params)
+    if @payment_purchase.valid?
+      @payment_purchase.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def payment_purchase_params
+    params.require(:payment_purchase).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
   end
 end
