18f377d14cfe76784629fd7f78c505c5c63242c2
NatsumiYonemichi <123153529+NatsumiYonemichi@users.noreply.github.com>
2023-05-28 13:58:54 +0900

    購入状況による表示の切り替え

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,7 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_root, only: :index
 
   def index
     @payment_purchase = PaymentPurchase.new
@@ -23,5 +25,15 @@ class PurchasesController < ApplicationController
 
   def payment_purchase_params
     params.require(:payment_purchase).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
+
+  def move_to_root
+    unless current_user == @item.user
+      if @item.purchase.present?
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
